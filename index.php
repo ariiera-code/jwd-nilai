66764d7209ba1f940fa4aaf2bd0e54e80270996b
ariiera-code <nibrosmaverick3@gmail.com>
2021-08-28 16:17:34 +0700

    Fix login form

--- index.php
+++ index.php
@@ -2,96 +2,75 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Source CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/lostyles.css">
-    <link rel="stylesheet" href="css/boxicons.min.css">
-    <!--aos-->
-    <link rel="stylesheet" href="css/aos.css">
+  <!-- Source CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/lostyles.css">
+  <link rel="stylesheet" href="css/boxicons.min.css">
+  <!--aos-->
+  <link rel="stylesheet" href="css/aos.css">
 
 
-    <title>Login Form</title>
+  <title>Login Form</title>
 
 </head>
 
 
 <body>
-<<<<<<< HEAD
-    <div id="login">
-        <h3 class="text-center text-white pt-5">Login form</h3>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" method="post" action="cek.php">
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="username" id="username" class="form-control" >
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
-                                <input type="password" name="password" id="password" class="form-control" >
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
-=======
 
-    <div class="row m-0 p-0">
-        <div class="col-6 d-none d-md-block pl-0">
-            <img src="img/login/pic.svg" alt="login" class="img">
-        </div>
-        <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
-            <div class="col-12">
-                <div class="text-center">
-                    <div class="text">    
-                        <img src="img/portal/logo.png" class="logo">
-                        <h1 class="atas h3">Halo! Selamat Datang!</h1>
-                    </div>
-                    <div class="text-input text-left pt-4">
-                        <div class="form-group"><label>Username</label>
-                            <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Enter username" required="yes">
-                        </div>
-                        <div class="form-group"><label>Password</label>
-                            <div class="input-group">
-                                <input type="password" name="password" id="password" class="form-control" data-toggle="password" placeholder="Password" required="yes">
->>>>>>> 236c1b5e10ea532ae308d451fc5312ff5e16e1e9
-                            </div>
-                        </div>
-                        <div class="form-row pt-4">
-                            <div class="col-1"></div>
-                            <div class="col-10 border d-flex justify-content-center"style="background-color:#f19933; cursor:pointer; height: 60px;">
-                                <div class="text-center font-weight-bold text-light my-auto">Login
-                                </div>
-                            </div> 
-                            <div class="col-1"></div>
-                        </div>
-                    </div>
-                </div>
-</div>
-            
-        </div>
+  <div class="row m-0 p-0">
+    <div class="col-6 d-none d-md-block pl-0">
+      <img src="img/login/pic.svg" alt="login" class="img">
     </div>
+    <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+      <div class="col-12">
+        <div class="text-center">
+          <div class="text">
+            <img src="img/portal/logo.png" class="logo">
+            <h1 class="atas h3">Halo! Selamat Datang!</h1>
+          </div>
+          <div class="text-input text-left pt-4">
+            <div class="form-group"><label>Username</label>
+              <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Enter username" required="yes">
+            </div>
+            <div class="form-group"><label>Password</label>
+              <div class="input-group">
+                <input type="password" name="password" id="password" class="form-control" data-toggle="password" placeholder="Password" required="yes">
+              </div>
+            </div>
+            <div class="form-row pt-4">
+              <div class="col-1"></div>
+              <div class="col-10 border d-flex justify-content-center" style="background-color:#f19933; cursor:pointer; height: 60px;">
+                <div class="text-center font-weight-bold text-light my-auto">Login
+                </div>
+              </div>
+              <div class="col-1"></div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/boxicons.js"></script>
-    <script src="js/jquery-3.5.1.slim.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/style.js"></script>
-    <script type="text/javascript" src="js/mdb.min.js"></script>
-    <!--AOS-->
-    <script src="js/aos.js"></script>
-    <!--Install AOS-->
-    <script>
-        AOS.init();
-    </script>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/boxicons.js"></script>
+  <script src="js/jquery-3.5.1.slim.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/style.js"></script>
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!--AOS-->
+  <script src="js/aos.js"></script>
+  <!--Install AOS-->
+  <script>
+    AOS.init();
+  </script>
 
 </body>
 
